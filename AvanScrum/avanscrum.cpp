5fe03835070f479318ccdc5cd15047622b1c72f5
willemjan92 <willemjan92@live.nl>
2013-11-25 18:56:02 +0100

    Projecten ophalen en in dropdown gezet

--- AvanScrum/avanscrum.cpp
+++ AvanScrum/avanscrum.cpp
@@ -2,18 +2,43 @@
 #include "qmessagebox.h";
 #include "TFS\sprint.h";
 #include <iostream>
+#include "TFS\TFSTransaction.h"
+#include "TFS\Project.h"
+#include "TFS\Sprint.h"
+#include "TFS\Defect.h"
+#include "TFS\SprintBacklogItem.h"
+#include "TFS\User.h"
 
 QPushButton *button;
 
 AvanScrum::AvanScrum(QWidget *parent) : QMainWindow(parent)
 {
 	ui.setupUi(this);
+	
+    std::list<std::string> saFilenameList;
+    std::list<std::string>::iterator iList;
+	QStringList *sl = new QStringList();
+	QString sFilename;
+    int y = 0;
 
-	QStringList *s = new QStringList();
-	s->insert(0,"project 1");
-	s->insert(1,"project 2");
-	s->insert(2,"project 3");
-	ui.cb_Projects_3->addItems(*s);
+    try
+    {
+        TFSTransaction::remoteListProjects(saFilenameList);
+
+        for (iList = saFilenameList.begin(); iList != saFilenameList.end(); ++iList)
+        {
+            sFilename = iList->c_str();
+			sl->insert(y, sFilename);
+            y++;
+		}
+    }
+    catch ( ... )
+    {
+		QMessageBox msgBox;
+		msgBox.setText("Geen verbinding met de server...");
+		msgBox.setStandardButtons(QMessageBox::Ok);
+    }
+	ui.cb_Projects_3->addItems(*sl);
 
 	button = ui.btn_NextSprint_3;
 
@@ -34,6 +59,6 @@ void AvanScrum::handleButton()
 	QMessageBox msgBox;
 	msgBox.setText(sprint->getName());
 	msgBox.setStandardButtons(QMessageBox::Ok);
-
+	
 	msgBox.exec();
 }
